80ca3004c7a7a1a8622118a7b832faf4fc9319c8
Ben <bsmith20681@gmail.com>
2020-03-15 11:47:19 -0600

    added authentication to backend and installed next.js on frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+//bring routes
+const blogRoutes =  require('./routes/blog');
+const authRoutes =  require('./routes/auth');
+
 // app
 const app = express();
 
@@ -23,10 +27,10 @@ if (process.env.NODE_ENV === 'development'){
   app.use(cors({origin: `${process.env.CLIENT_URL}`}));
 }
 
-// routes
-app.get('/api', (req, res) => {
-    res.json({ time: Date().toString() });
-});
+//routes middleware
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+
 
 // port
 const port = process.env.PORT || 8000;
